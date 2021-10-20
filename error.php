69e163fdc280b2781c51baf594611688abf43e57
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2021-10-20 10:46:40 +0200

    oculta el menu segun la hora

--- error.php
+++ error.php
@@ -7,12 +7,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="normalize.css">
     <title>Error</title>
+    <style>
+        .pantalla{
+            height: 80vh; /* For 100% screen height */
+            width:  80vw; /* For 100% screen width */
+        }
+    </style>
 </head>
 
 <body>
-    <?php include("header.php") ?>  
+    <?php include("header.php") ?>
+    <div class="pantalla">
     <h1>Error</h1>
-
+    </div>
 
     <?php include ("footer.php"); ?>
 </body>
