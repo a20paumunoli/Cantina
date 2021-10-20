3c81bb4766ff2e09531d474b4e292d1b19a46ce2
ASandraOrtiz <a20sanortveg@inspedralbes.cat>
2021-10-20 11:42:41 +0200

    ok

--- formulariDades.php
+++ formulariDades.php
@@ -8,6 +8,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="normalize.css">
     <title>Validació comanda</title>
+
+    <style>
+
+        .formulari{
+            margin-left: 20px;
+            margin-top:50px;
+        }
+
+    </style>
+
+
+
 </head>
 
 <body>
@@ -16,46 +28,85 @@
 
     <!--Mostrar preu total compra-->
     
-    <form method="POST" action="ticket.php">
+    <form method="post" name="form" action="ticket.php">
 
-        <div id="formulari" >
+        <div class="formulari" >
             <div>
                 <label for="name">Nom</label>
-                <input name="name" type="text" id="name" required>
+                <input name="name" type="text" id="nom" value="">
             </div>
         <br>
             <div>
                 <label for="tlf">Teléfon</label>
-                <input name="tlf" type="tel" id="tlf" size="9" placeholder="+34 XXXXXXXXX" width="10px" required>
+                <input name="tlf" type="tel" id="tlf" size="9" placeholder="+34 XXXXXXXXX" width="10px" >
             </div>
         <br>
             <div>
                 <label for="email">Correu electrònic </label>
-                <input name="email" type="email" maxlength="50" placeholder="nom@inspedralbes.cat" required/>
+                <input name="email" type="email" id="correu" maxlength="50" placeholder="nom@inspedralbes.cat" />
             </div>
-        </div>
 
         <br>
-
-        <div id="buttons">
-            <input type="submit" value="Comprar">
-            <input type="reset" value="Neteja">
+            <div class="buttons">
+                <input type="submit" value="Comprar" id="submit">
+                <input type="reset" value="Neteja">
+            </div>
         </div>
 
     </form>
 
-<!--
+
     <script>
-        function validarEmail(email) {
-            if ("/^[a-zA-Z0-9._-]+@inspedralbes\.cat$/".test(email)){
-            else{
-            alert("La dirección de email es incorrecta!.");
+
+        var nom = document.getElementById("nom");
+        var submit = document.getElementById("submit");
+
+        window.onload = function(){
+		    nom.focus();
+	    }
+
+        submit.addEventListener("click", function(e){
+            var n = 0; 
+            if(comprovarNom(nom) == 1){
+                e.preventDefault();
+                alert("Introdueix nom");
             }
-            }   
-    
-    }
+            if(comprovarTel()){
+                e.preventDefault();
+                alert("Introdueix nom");
+            }
+            if(comprovarEmail()){
+                e.preventDefault();
+                alert("Introdueix nom");
+            }
+
+
+            function comprovarNom(nom) {
+                if (nom.value == "a"){
+                    n = 1;
+                }
+                return n;
+            }
+            
+            
+            function comprovarEmail() {
+                if ("/^[a-zA-Z0-9._-]+@inspedralbes\.cat$/".test(email)){
+                else{
+                    alert("La dirección de email es incorrecta!.");
+                }
+            } 
+        }
+
+           
+        });
+
+
     </script>
--->
+
+
+
+
+        
 
     <?php include ("footer.php"); ?>
 </body>
