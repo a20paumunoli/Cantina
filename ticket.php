5dad361c5ccf24cf72cff9217a8260c1890037f1
Sandra Ortiz Vega <a20sanortveg@inspedralbes.cat>
2021-10-25 22:59:47 +0200

    css

--- ticket.php
+++ ticket.php
@@ -19,8 +19,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- CSS -->
     <link rel="stylesheet" href="normalize.css">
-    <link type="text/css" rel="stylesheet" href="header.css">
+    <link type="text/css" rel="stylesheet" href="css/header.css">
     <title>Ticket</title>
     <style>
             .separate{
