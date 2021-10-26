0c920e9662bd715283fe75b4af1a3a24e2e90e92
a20paumunoli <a20paumunoli@inspedralbes.cat>
2021-10-26 20:53:35 +0200

    Canvios

--- client/header.php
+++ client/header.php
@@ -2,13 +2,10 @@
     <div id="h-1">        
         <a href="index.php"><img id="logo" src="img/logo.png"  width="60px"> </a>
     </div>
-
     <div id="h-2">
         <h1>Cantina<span class="sp2"> de l'Ins Pedralbes</span></h1> 
     </div>
-
     <div id="h-3"> 
         <button href="index.php" class="boton">Home</button>            
     </div>
-
 </div>
