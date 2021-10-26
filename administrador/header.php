f57cecb43acfa49e4a02aa12dcef28fe5e3e0363
Sandra Ortiz Vega <a20sanortveg@inspedralbes.cat>
2021-10-26 23:22:05 +0200

    .

--- administrador/header.php
+++ administrador/header.php
@@ -1,11 +1,11 @@
 <div id="h-cont" class="alert alert-secondary">
     <div id="h-1">        
-        <a href="index.php"><img id="logo" src="/client/img/logo.png" width="60px"> </a>
+        <a href="../index.php"><img id="logo" src="../client/img/logo.png" width="60px"> </a>
     </div>
     <div id="h-2">
         <p class="n">Cantina<span class="sp2"> de l'Ins Pedralbes</span></p> 
     </div>
     <div id="h-3"> 
-        <button href=".//index.php" class="boton">Home</button>            
+        <button href="../index.php" class="boton">Home</button>            
     </div>
 </div>
