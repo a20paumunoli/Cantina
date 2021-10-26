62a7eea68a576b39d16466c3a650fbefd0fcdb2b
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2021-10-26 10:30:44 +0200

    modificant admin

--- administrador/admin.php
+++ administrador/admin.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="normalize.css">
-    <link type="text/css" rel="stylesheet" href="css/header.css">
+    <link type="text/css" rel="stylesheet" href="../client/css/header.css">
+    <link type="text/css" rel="stylesheet" href="../client/css/footer.css">
     <title>Admin</title>
     <style>
 
@@ -14,23 +15,32 @@
             height: 81vh;
             display: flex;
             text-align: center;
-
+            justify-content:center
         }
-        .novetats{
+        .novetats1{
             background-color: #E2E3E5;
             width: 750px;
             height: 700px;
             text-align: center;
             border: 1px black solid;
+            background-image: url("img/pizarra.jpg");
+            -webkit-background-size: 700px;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+
+        }
+        img{
+            width: 600px;
+            height: 600px;
         }
     </style>
 
 </head>
     <body>
-    <?php include("header.php") ?>
+    <?php include("../client/header.php") ?>
 
     <div class=pantalla>
-        <div class="novetats">
+        <div class="novetats1">
             <h2>Consultar Comandes</h2>
         </div>
         <div class="novetats">
@@ -39,6 +49,7 @@
     </div>
 
 
-    <?php include ("footer.php"); ?>
+    <?php include ("../client/footer.php"); ?>
 </body>
+
 </html>
