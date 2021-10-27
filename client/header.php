f8666515aa74b4bfb2f430932c92fcf6956c858c
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2021-10-27 08:36:37 +0200

    link arreglado

--- client/header.php
+++ client/header.php
@@ -1,6 +1,6 @@
 <div id="h-cont" class="alert alert-secondary">
     <div id="h-1">        
-        <a href="../index.php"><img id="logo" src="/Cantina/client/img/logo.png" width="60px"> </a>
+        <a href="/Cantina/index.php"><img id="logo" src="/Cantina/client/img/logo.png" width="60px"> </a>
     </div>
     <div id="h-2">
         <p class="n">Cantina<span class="sp2"> de l'Ins Pedralbes</span></p> 
